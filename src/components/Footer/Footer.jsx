738f0209939e5f4f1cd644176157e7fc3959f29c
Abstract Programmer <90398694+samuelteklemariam@users.noreply.github.com>
2024-02-09 10:34:04 -0600

    frontend update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import './Footer.css'
-import footer_logo from '../Assets/logo_big.png'
+// import footer_logo from '../Assets/logo_big.png'
 import instagram_icon from '../Assets/instagram_icon.png'
 import pintester_icon from '../Assets/pintester_icon.png'
 import whatsapp_icon from '../Assets/whatsapp_icon.png'
+import logo from '../Assets/logo.png'
+
+
 
 const Footer = () => {
   return (
     <div className='footer'>
         <div className="footer-logo">
-            <img src={footer_logo} alt="" />
-            <p>SHOPPER</p>
+            <img src={logo} alt="" />
+            {/* <p>SHOPPER</p> */}
         </div>
         <ul className='footer-links'>
             <li>Company</li>
